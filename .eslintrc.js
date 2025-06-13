48f666fc6b6e79665f99eeb5aa57292a61ac72da
Kit Allen <kitallen23@gmail.com>
2025-06-13 13:16:46 +1000

    Chore: updated eslint config to be more modern

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: ["@typescript-eslint/recommended", "prettier"],
-    plugins: ["@typescript-eslint", "prettier"],
-    rules: {
-        "prettier/prettier": "error",
-        "no-console": ["warn", { allow: ["info", "warn", "error"] }],
-    },
-};
